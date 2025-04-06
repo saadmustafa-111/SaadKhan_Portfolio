bc0b2c54014581ddc53460691b9275977995480d
malahimamir <malahimamir@gmail.com>
2025-04-06 09:56:13 +0500

    fint changes

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-6 md:p-12 flex flex-col md:flex-row items-center justify-between gap-4 text-center md:text-left">
-        <span className="text-lg font-semibold">malahimaamir@gmail.com</span>
+        <span className="text-lg text-slate-600 font-semibold">malahimaamir@gmail.com</span>
         <p className="text-slate-600 text-sm">© 2024 Malahima Amir | Bringing ideas to life.</p>
         <Footericon />
       </div>
